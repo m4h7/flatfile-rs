a673a11910666c11be5b74ca3874954764fb3247
mhg <mhg@urml.eu>
2018-03-02 07:06:48 +0000

    Make schema_write functions public

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,4 @@ pub use v1::schema::Metadata;
 pub use v1::parse::parse_string;
 pub use types::ColumnValue;
 pub use v2::mmapbuf::MmapBuf;
-pub use v2::write2::{read_schema_v2, schema_read_row};
+pub use v2::write2::{read_schema_v2, schema_read_row, write_schema_v2, schema_write};
