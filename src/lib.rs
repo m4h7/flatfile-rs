349faadf5f98eacf641bdae80cd513a300809d45
mhg <mhg@urml.eu>
2018-03-02 13:05:30 +0000

    Add ColumnType to root

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod v1;
 pub mod v2;
 pub use v1::schema::Metadata;
 pub use v1::parse::parse_string;
-pub use types::ColumnValue;
+pub use types::{ColumnType, ColumnValue};
 pub use v2::mmapbuf::MmapBuf;
 pub use v2::write2::{read_schema_v2, schema_read_row, write_schema_v2, schema_write};
 pub use v2::filebuf::FileBuf;
