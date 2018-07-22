1f4870f84f16f1590b515734494982000ad8ba8d
mhg <mhg@urml.eu>
2018-07-22 20:10:25 +0100

    flatfile.h: reformat with clang-format, style goog, tab 4

--- include/flatfile.h
+++ include/flatfile.h
@@ -9,22 +9,13 @@
 unsigned long schema2_create();
 int schema2_len(unsigned int schema_handle);
 void schema2_destroy(unsigned int schema_handle);
-int schema2_add_column(unsigned long schema_handle,
-                       char const* name,
-                       char const* ctype,
-                       _Bool nullable);
-int schema2_get_column_name(unsigned int schema_handle,
-                            int index,
-                            char* buf);
+int schema2_add_column(unsigned long schema_handle, char const* name,
+                       char const* ctype, _Bool nullable);
+int schema2_get_column_name(unsigned int schema_handle, int index, char* buf);
 
-int schema2_get_column_type(unsigned int schema_handle,
-                            int index,
-                            char* buf);
+int schema2_get_column_type(unsigned int schema_handle, int index, char* buf);
 
-bool schema2_get_column_nullable(
-    unsigned int schema_handle,
-    int index
-);
+bool schema2_get_column_nullable(unsigned int schema_handle, int index);
 
 /**
  * write functions
@@ -36,18 +27,19 @@ void writef_close(unsigned int handle);
 unsigned int writef_get_schema(unsigned int handle);
 
 void writef_row_start(unsigned int handle);
-void writef_row_set_u32(
-    unsigned int handle,
-    unsigned int index,
-    unsigned int value
-    );
-void writef_row_set_u64(unsigned int handle, unsigned int index, unsigned long value);
-void writef_row_set_string(unsigned int handle, unsigned int index, char const* s);
+void writef_row_set_u32(unsigned int handle, unsigned int index,
+                        unsigned int value);
+void writef_row_set_u64(unsigned int handle, unsigned int index,
+                        unsigned long value);
+void writef_row_set_string(unsigned int handle, unsigned int index,
+                           char const* s);
 bool writef_row_end(unsigned int handle);
 void writef_flush(unsigned int handle);
 
-unsigned long readf_row_get_string_len(unsigned int fhandle, unsigned int index);
-unsigned long readf_row_get_string(unsigned int fhandle, unsigned int index, void* out, unsigned long size);
+unsigned long readf_row_get_string_len(unsigned int fhandle,
+                                       unsigned int index);
+unsigned long readf_row_get_string(unsigned int fhandle, unsigned int index,
+                                   void* out, unsigned long size);
 unsigned long readf_row_get_u64(unsigned int fhandle, unsigned int index);
 unsigned int readf_row_get_u32(unsigned int fhandle, unsigned int index);
 unsigned int readf_row_is_null(unsigned int fhandle, unsigned int index);
@@ -56,4 +48,4 @@ void readf_close(unsigned int fhandle);
 unsigned int readf_open(char const* name);
 unsigned int readf_clone_schema(unsigned int fhandle);
 
-#endif // FLATFILE_H_INCLUDED
+#endif  // FLATFILE_H_INCLUDED
