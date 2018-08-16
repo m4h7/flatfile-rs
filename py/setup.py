1b633de5c7fa859d7a96666e45860e3373cda052
mhg <mhg@urml.eu>
2018-08-16 08:14:11 +0100

    py: 1.1.6

--- py/setup.py
+++ py/setup.py
@@ -8,7 +8,7 @@ module1 = Extension('_flatfile',
                     sources = ['flatfile.c'])
 
 setup(name='yrml-flatfile',
-      version='1.1.5',
+      version='1.1.6',
       description='flatfile package',
       packages=["flatfile"],
       ext_modules=[module1])
