86474d23145ba64159c6f5671de331a0053224d3
mhg <mhg@urml.eu>
2019-06-01 10:39:56 +0100

    py: build release libs

--- py/test.sh
+++ py/test.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 set -e
-(cd ../rs; cargo build; cd -)
-(cd ../clib; cargo build; cd -)
+(cd ../rs; cargo build; cargo build --release; cd -)
+(cd ../clib; cargo build; cargo build --release; cd -)
 rm -rf build
 python3 setup.py build
 cp build/lib.macosx-10.13-x86_64-3.7/_flatfile.cpython-37m-darwin.so ./
