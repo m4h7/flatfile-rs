b7ae09e32e96610a247660a4731d49f3ad58004b
mhg <mhg@urml.eu>
2019-09-01 06:13:38 +0200

    changed manifest.in flatfile.h path to point to the symlink

--- py/setup.py
+++ py/setup.py
@@ -8,7 +8,7 @@ module1 = Extension('_flatfile',
                     sources = ['flatfile.c'])
 
 setup(name='yrml-flatfile',
-      version='1.3.11',
+      version='1.3.13',
       description='flatfile package',
       packages=["flatfile"],
       ext_modules=[module1])
