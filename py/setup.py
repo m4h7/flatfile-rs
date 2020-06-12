1e91ff4637144d406b93120838bda6af839c5be6
mhg <mhg@mm.st>
2020-06-12 09:33:28 +0000

    py: inc version

--- py/setup.py
+++ py/setup.py
@@ -8,7 +8,7 @@ module1 = Extension('_flatfile',
                     sources = ['flatfile.c'])
 
 setup(name='yrml-flatfile',
-      version='1.3.14',
+      version='1.3.15',
       description='flatfile package',
       packages=["flatfile"],
       ext_modules=[module1])
